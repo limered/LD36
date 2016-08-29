e6cb4a756a3dd9da969ae0694c7a2c422251a684
Michael Scherbakow <ryupold@gmail.com>
2016-08-29 23:06:20 +0200

    cooking meat

--- Assets/Scripts/Behaviours/InflameableByFire.cs
+++ Assets/Scripts/Behaviours/InflameableByFire.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
 
-[RequireComponent(typeof(Fuel))]
 public class InflameableByFire : MonoBehaviour
 {
+    public Fuel fuel;
     public float timeUntilStartsBurning = 3f;
     [Range(1, 100)]
     public int minFireCount = 1;
     public bool firesMultiply = false;
-    private float timeUnderFire = 0f;
 
+    private float timeUnderFire = 0f;
     private readonly List<Collider> onFireBy = new List<Collider>();
     private int fireLayer;
     private readonly CompositeDisposable burningStatusDisposable = new CompositeDisposable();
@@ -21,13 +20,14 @@ public class InflameableByFire : MonoBehaviour
     void Start()
     {
         fireLayer = LayerMask.NameToLayer("Fire");
+        fuel = fuel ? fuel : GetComponent<Fuel>();
     }
 
     private void StartBurning()
     {
-        if (!burningStatusDisposable.Any() && !GetComponent<IsBurning>())
+        if (!burningStatusDisposable.Any() && !fuel.gameObject.GetComponent<IsBurning>())
         {
-            gameObject.AddComponent<IsBurning>()
+            fuel.gameObject.AddComponent<IsBurning>()
                 .OnDestroyAsObservable()
                 .Subscribe(x => burningStatusDisposable.Clear())
                 .AddTo(burningStatusDisposable);
